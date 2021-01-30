656b5524127de4ef92bd017a5263732e2efdd723
Simay Ekici <43034579+siimaaay25@users.noreply.github.com>
2021-01-31 02:31:02 +0300

    admin can move the user

---
+++ editMove.php
@@ -0,0 +1,160 @@
+<?php
+
+include ('dbconnection.php'); // Using database connection file here
+
+$id = $_GET['id'];
+
+$qry = mysqli_query($con,"select * from tblvisitor where userID='$id'"); // select query
+
+$data = mysqli_fetch_array($qry); // fetch data
+
+if(isset($_POST['update'])) // when click on Update button
+{
+    $isActive = $_POST['isActive'];
+    
+
+    $edit = mysqli_query($con,"update tblvisitor set isActive='0' where userID='$id'");
+	
+    if($edit)
+    {
+		$sql="INSERT INTO wholeft (username, VisitorName, MobileNumber, EnterDate)
+SELECT username, VisitorName, MobileNumber,EnterDate
+FROM tblvisitor 
+WHERE isActive='0'; ";
+		$qry = mysqli_query($con,$sql); // select query
+
+		
+		
+        mysqli_close($con); // Close connection
+        header("location:manage-newresident.php"); // redirects to all records page
+        exit;
+    }
+    else
+    {
+        echo mysqli_error();
+    }    	
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Lara Tower Resident Forms</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <!-- HEADER MOBILE-->
+        <?php include_once('includes/userSidebar.php');?>
+   
+        <div class="page-container">
+            <!-- HEADER DESKTOP-->
+            <?php include_once('includes/header.php');?>
+            <!-- HEADER DESKTOP-->
+
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+                          
+                            <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <strong>Add</strong> New Payment
+                                    </div>
+                                    <div class="card-body card-block">
+                                        <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                                            <p style="font-size:16px; color:red" align="center">
+     </p>
+                                            <div class="row form-group">
+                                                <div class="col col-md-3">
+                                                    <label name="isActive" for="text-input" class=" form-control-label">Do you want to move out the user?(y/n) </label>
+                                                </div>
+                                                <div class="col-12 col-md-9">
+                                                    <input type="text" id="isActive" name="isActive" placeholder="Please type y for Yes,n for No" class="form-control" required="true">
+                                                    
+                                                </div>
+                                            </div>
+                                            
+                                          
+                                          <div class="card-footer">
+                                        <p style="text-align: center;"><button type="submit" name="update" id="update" class="btn btn-primary btn-sm">Add
+                                        </button></p>
+                                        
+                                    </div>
+                                        </form>
+                                    </div>
+                                   
+                                </div>
+                       
+                        </div>
+                        
+                    </div>
+               
+ 
+<?php include_once('includes/footer.php');?>
+   </div> </div>
+            </div>
+        </div>
+</div>
+    <!-- Jquery JS-->
+    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="vendor/slick/slick.min.js">
+    </script>
+    <script src="vendor/wow/wow.min.js"></script>
+    <script src="vendor/animsition/animsition.min.js"></script>
+    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
+<?php  ?>
